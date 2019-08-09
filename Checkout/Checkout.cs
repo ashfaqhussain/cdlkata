d52250ff95916026d8e8420d7132bb7f98102785
Ashfaq Hussain <ash2093@hotmail.com>
2019-08-09 12:34:26 +0100

    Declarative pricing strategy

--- Checkout/Checkout.cs
+++ Checkout/Checkout.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Checkout
 {
     public class Checkout : ICheckout
     {
         private readonly List<string> _items = new List<string>();
+        private readonly Pricing _pricing = new Pricing();
 
         public void Scan(string item)
         {
@@ -13,10 +15,21 @@ namespace Checkout
 
         public int GetTotalPrice()
         {
-            var groups = _items.Count / 3;
-            var units = _items.Count - (groups * 3);
+            var total = 0;
 
-            return (groups * 130) + (units * 50);
+            var itemGroups = _items.GroupBy(x => x);
+
+            foreach (var itemGroup in itemGroups)
+            {
+                var price = _pricing.Items.First(x => x.SKU == itemGroup.Key);
+
+                var specialPriced = itemGroup.Count() / price.SpecialPriceQuantity;
+                var unitPriced = itemGroup.Count() - (specialPriced * price.SpecialPriceQuantity);
+
+                total += (specialPriced * price.SpecialPrice) + (unitPriced * price.UnitPrice);
+            }
+
+            return total;
         }
     }
 }
---
+++ Checkout/Pricing.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Checkout
+{
+    public class Pricing
+    {
+        public List<Price> Items;
+
+        public Pricing()
+        {
+            Items = new List<Price>
+                    {
+                        new Price("A", 50, 3, 130),
+                        new Price("B", 30, 2, 45),
+                        new Price("C", 20),
+                        new Price("D", 15)
+                    };
+        }
+    }
+
+    public class Price
+    {
+        public Price(string sku, int unitPrice, int specialPriceQuantity, int specialPrice)
+        {
+            SKU = sku;
+            UnitPrice = unitPrice;
+            SpecialPriceQuantity = specialPriceQuantity;
+            SpecialPrice = specialPrice;
+        }
+        public Price(string sku, int unitPrice) : this(sku, unitPrice, 0, 0)
+        {
+        }
+
+        public readonly string SKU;
+        public readonly int UnitPrice;
+        public readonly int SpecialPriceQuantity;
+        public readonly int SpecialPrice;
+    }
+}
